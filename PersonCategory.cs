536bc5212a029c28cf8de33f23917a9cac9a1b0b
puneethcm <puneethkumarcm8050@gmail.com>
2023-04-13 17:50:12 +0530

    PersonAge

---
+++ PersonCategory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace ExceptionDemo
+{
+	public class PersonCategory
+	{
+		public static void FindPersonCategory(int age)
+		{
+			if(age>0 && age <= 14)
+			{
+				Console.WriteLine("The Person He/She is Children Cotegory");
+			}
+			else if(age >= 15 && age <= 24)
+			{
+                Console.WriteLine("The Person He/She is Youth Cotegory");
+            }
+            else if (age >= 25 && age <= 64)
+            {
+                Console.WriteLine("The Person He/She is Adults Cotegory");
+            }
+			else
+			{
+                Console.WriteLine("The Person He/She is Seniors Cotegory");
+            }
+        }
+	}
+}
+
--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
-﻿internal class Program
+﻿using ExceptionDemo;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Exception Demo");
+        Console.WriteLine("Enter person Age");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        PersonCategory.FindPersonCategory(age);
         Console.ReadLine();
     }
 }
